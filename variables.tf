d14d9abc6de8b9e5b894d65d6a5c2f8728ec8cab
SaratA8717 <115520069+SaratA8717@users.noreply.github.com>
2024-06-03 15:03:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+  type        = string
+  description = "The name of the Resource Group"
+}
+
+variable "rglocation" {
+  type        = string
+  description = "The name of the Location "
+}
