a3354d453c0d2c0096529bce5c375d40a05e4c10
SaratA8717 <115520069+SaratA8717@users.noreply.github.com>
2024-06-11 11:06:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 resource "azurerm_resource_group" "resource_group_eus2" {
   name     = "rg-${var.rgname}"
   location = var.rglocation
-  tags = var.eus2_tags
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "sa${var.app}${var.env}"
+  resource_group_name      = var.resourcegroup
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "test"
+  }
 }
