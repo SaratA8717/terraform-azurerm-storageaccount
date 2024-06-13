ae1dfc2ae39b40bbc91ae58763703c494a276392
SaratA8717 <115520069+SaratA8717@users.noreply.github.com>
2024-06-13 09:51:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,6 @@ resource "azurerm_storage_account" "example" {
   name                     = "sa${var.app}${var.env}"
   resource_group_name      = var.resourcegroup
   location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "test"
-  }
+  account_tier             = var.storagetier
+  account_replication_type = var.redunancy
 }
