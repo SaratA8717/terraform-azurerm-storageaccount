ca7ef3337a84ae7692083704c5d8691a7a18fb47
SaratA8717 <115520069+SaratA8717@users.noreply.github.com>
2024-06-18 14:22:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "app" {
-  type        = string
-  description = "Name of your application"
-}
-
 variable "location" {
   type        = string
   description = "The name of the Location to deploy resource"
@@ -10,10 +5,15 @@ variable "location" {
 
 variable "storagetier" {
   type        = string
-  description = "Provide Storage Tier"
+  description = "Provide Storage Tier Standard or Preimum"
 }
 
 variable "storage_redunancy" {
   type        = string
-  description = "The name of the Location "
+  description = "The name of the Storage Redunancy - LRS, GRS "
+}
+
+variable "resourcegroup" {
+  type        = string
+  description = "The name of the RG "
 }
